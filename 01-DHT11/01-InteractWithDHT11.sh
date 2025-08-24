62546d20c8ee98aab6fc389caadf65510934d573
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2025-08-24 10:19:19 +0000

    cleanup

--- 01-DHT11/01-InteractWithDHT11.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-sudo apt install -y python3
-sudo apt install -y python3-pip
-python3 -m pip install --break-system-packages  adafruit-circuitpython-dht==3.5.5
-python3 InteractWithDHT11.py
---
+++ 01-DHT21/01-InteractWithDHT21.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+sudo apt install -y python3
+sudo apt install -y python3-pip
+python3 -m pip install  adafruit-circuitpython-dht
+python3 InteractWithDHT21.py
